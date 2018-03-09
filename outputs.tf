a8154e4655c992b7b7b831e7b8c75e382b12dbe1
Yuya Ma'emichi <maemichi@monosense.co.jp>
2018-03-09 19:02:24 +0900

    Add autoscaling resource with module input/output

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+# Module outputs
+
+output "autoscaling_name_map" {
+  value = {
+    read  = "${aws_appautoscaling_policy.dynamodb_read.name}"
+    write = "${aws_appautoscaling_policy.dynamodb_write.name}"
+  }
+}
