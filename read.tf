58cbd86343d3d11c5932227ce04e6e1703c439ce
Casey Wireman <cwire4@gmail.com>
2020-10-15 18:11:47 -0400

    use floor since it wants an int

--- read.tf
+++ read.tf
@@ -3,13 +3,16 @@
 resource "aws_appautoscaling_target" "dynamodb_read" {
   resource_id = local.resource_id
 
-  min_capacity = var.read_min
-  max_capacity = var.read_max
+  min_capacity = floor(var.read_min)
+  max_capacity = floor(var.read_max)
 
   service_namespace  = local.namespace
   scalable_dimension = "${local.namespace}:${local.type}:${local.resource_name["read"]}"
 
   role_arn = data.aws_iam_role.dynamodb_autoscale_role.arn
+  lifecycle {
+    ignore_changes = [max_capacity]
+  }
 }
 
 resource "aws_appautoscaling_policy" "dynamodb_read" {
--- write.tf
+++ write.tf
@@ -3,13 +3,16 @@
 resource "aws_appautoscaling_target" "dynamodb_write" {
   resource_id = local.resource_id
 
-  min_capacity = var.write_min
-  max_capacity = var.write_max
+  min_capacity = floor(var.write_min)
+  max_capacity = floor(var.write_max)
 
   service_namespace  = local.namespace
   scalable_dimension = "${local.namespace}:${local.type}:${local.resource_name["write"]}"
 
   role_arn = data.aws_iam_role.dynamodb_autoscale_role.arn
+  lifecycle {
+    ignore_changes = [max_capacity]
+  }
 }
 
 resource "aws_appautoscaling_policy" "dynamodb_write" {
